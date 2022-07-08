4f93c0e5159e2d342375826dc76e776904916fd6
anarakinson <anarakinson@gmail.com>
2022-07-08 13:19:44 +0300

    task about speed

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,32 +2,14 @@ package main
 
 import (
     "fmt"
-    "math"
 )
 
 func main() {
-    m := map[string]float32{"first": 1, "second": 2, "third": 3}
-    fmt.Println(m)
-    
-    x, ok := m["fourth"]
-    if !ok {
-        fmt.Println(x, ok)
-    }
-
-    delete(m, "second")
-
-    fmt.Println(m)
-
-    m["fifth"] = 12
-    fmt.Println(m)
-
-    var a string = "hello"
-    fmt.Println(a)
-    var b = []rune(a)
-    b[2] = '!'
-    var c = string(b)
-    fmt.Println(c)
-
-    fmt.Println(fmt.Sprintf("%.2f", math.Round(9.7)))
-    fmt.Println(math.Pow(2, 2))
+    var AmericanVelocity, EuropeanVelocity float64
+    var speed float64 = 120.4 // m/sec
+    EuropeanVelocity = (speed / 1000) * 3600
+    AmericanVelocity = (speed / 1609) * 3600
+    fmt.Println(fmt.Sprintf("%.2f m/s", speed))
+    fmt.Println(fmt.Sprintf("%.2f km/h", EuropeanVelocity))
+    fmt.Println(fmt.Sprintf("%.2f mph", AmericanVelocity))
 }
