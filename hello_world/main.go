e060d96ad1d7824fa0f42053c077bff5d900c57b
anarakinson <anarakinson@gmail.com>
2022-07-08 13:20:56 +0300

    task about speed

--- hello_world/main.go
+++ hello_world/main.go
@@ -9,6 +9,7 @@ func main() {
     var speed float64 = 120.4 // m/sec
     EuropeanVelocity = (speed / 1000) * 3600
     AmericanVelocity = (speed / 1609) * 3600
+    
     fmt.Println(fmt.Sprintf("%.2f m/s", speed))
     fmt.Println(fmt.Sprintf("%.2f km/h", EuropeanVelocity))
     fmt.Println(fmt.Sprintf("%.2f mph", AmericanVelocity))
